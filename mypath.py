a881a3b57636bc5a895c71c4e923739eddc03471
omarorajain <92062635+omarorajain@users.noreply.github.com>
2024-12-10 04:07:12 -0500

    Update paths

--- mypath.py
+++ mypath.py
@@ -3,10 +3,10 @@ class Path(object):
     def db_dir(database):
         if database == 'ucf101':
             # folder that contains class labels
-            root_dir = '/Path/to/UCF-101'
+            root_dir = '../UCF-101'
 
             # Save preprocess data into output_dir
-            output_dir = '/path/to/VAR/ucf101'
+            output_dir = '../ucf101'
 
             return root_dir, output_dir
         elif database == 'hmdb51':
@@ -22,4 +22,4 @@ class Path(object):
 
     @staticmethod
     def model_dir():
-        return '/path/to/Models/c3d-pretrained.pth'
+        return '../c3d-pretrained.pth'
